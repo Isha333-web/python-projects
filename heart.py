4046b1a74001b8d3ed170adfc927ad15ff99b9d2
Isha <85048149+Isha333-web@users.noreply.github.com>
2022-09-29 12:43:14 +0530

    Create heart.py

---
+++ heart.py
@@ -0,0 +1,15 @@
+import turtle
+t=turtle.Turtle()
+
+t.color("red")
+t.begin_fill()
+t.fillcolor("pink")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
+
